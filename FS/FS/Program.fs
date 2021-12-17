feb278af16ed6d98e1df590126a34ef1e475001b
DevWz <wellington.zaneze@outlook.com.br>
2021-12-17 11:50:37 -0300

    initial commit and bin2Dec module

---
+++ FS/FS/Program.fs
@@ -0,0 +1,21 @@
+﻿
+module Bin2Dec =
+
+    let convert str =
+        
+        let array = [| for c in str do
+                                yield string c |]
+
+        let convert2 (col: 'string[]) =
+            [| for n = col.Length - 1 downto 0 do
+                    yield (int array[n] * pown 2 (array.Length - n - 1)) |]
+
+        Array.sum (convert2 array)
+
+[<EntryPoint>]
+let main argv =
+
+    let result = Bin2Dec.convert "100101"
+    printfn "%i" result
+
+    0
